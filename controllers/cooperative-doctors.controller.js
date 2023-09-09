4950aa66e5ba1427f1d5796ee508b8663c834bab
Eric Rowe <erowe43@gmail.com>
2023-09-09 00:39:03 -0500

    Save functionality

--- controllers/cooperative-doctors.controller.js
+++ controllers/cooperative-doctors.controller.js
@@ -1,4 +1,6 @@
+const mongoose = require("mongoose");
 const CooperativeDoctor = require('../models/cooperative-doctor.model');
+
 
 const getCooperativeDoctors = async (req, res) => {
     let doctors = await CooperativeDoctor.find().populate('primaryFacility');
@@ -21,11 +23,19 @@ const getCooperativeDoctor = async (req, res) => {
 }
 
 const createCooperativeDoctor = async (req, res) => {
+    req.body._id = new mongoose.Types.ObjectId();
+    req.body.firstname = req.body.fullname.split(" ")[0];
+    req.body.lastname = req.body.fullname.split(" ")[1] ? req.body.fullname.split(" ")[1] : "";
+    req.body.primaryPhone ? req.body.phoneNumbers.push(req.body.primaryPhone) : null;
     let doctor = await CooperativeDoctor.create(req.body);
     res.status(201).json(doctor);
 }
 
 const updateCooperativeDoctor = async (req, res) => {
+    req.body.firstname = req.body.fullname.split(" ")[0];
+    req.body.lastname = req.body.fullname.split(" ")[1] ? req.body.fullname.split(" ")[1] : "";
+    req.body.primaryPhone ? req.body.phoneNumbers = [req.body.primaryPhone] : req.body.phoneNumbers = [];
+    req.body.secondaryPhones ? req.body.phoneNumbers.push(req.body.secondaryPhones) : null;
     let doctor = await CooperativeDoctor.findByIdAndUpdate(req.params.id, req.body, { new: true });
     res.status(200).json(doctor);
 }
